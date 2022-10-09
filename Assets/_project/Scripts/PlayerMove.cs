9651af21514655696f294ce3c0266894334362d9
igoefi <96096968+igoefi@users.noreply.github.com>
2022-10-09 10:47:36 +0300

    Update move

--- Assets/_project/Scripts/PlayerMove.cs
+++ Assets/_project/Scripts/PlayerMove.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PlayerMove : MonoBehaviour
 {
     private Rigidbody2D _body;
 
+    [SerializeField] float _speed;
     void Start()
     {
         _body = GetComponent<Rigidbody2D>();
@@ -13,6 +15,9 @@ public class PlayerMove : MonoBehaviour
 
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.D))
+        {
+            _body.velocity += new Vector2(_speed * Time.deltaTime, 0);
+        }
     }
 }
