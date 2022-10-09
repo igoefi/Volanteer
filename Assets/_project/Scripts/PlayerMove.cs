b242631135cb7cde23e9275966cffe591e9824b6
igoefi <96096968+igoefi@users.noreply.github.com>
2022-10-09 11:27:40 +0300

    update movement

--- Assets/_project/Scripts/PlayerMove.cs
+++ Assets/_project/Scripts/PlayerMove.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -8,6 +9,10 @@ public class PlayerMove : MonoBehaviour
     private Rigidbody2D _body;
 
     [SerializeField] float _speed;
+    [SerializeField] float _maxSpeed;
+
+    [SerializeField] float _jumpForce;
+
     void Start()
     {
         _body = GetComponent<Rigidbody2D>();
@@ -15,9 +20,12 @@ public class PlayerMove : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKey(KeyCode.D))
-        {
-            _body.velocity += new Vector2(_speed * Time.deltaTime, 0);
-        }
+        _body.velocity += new Vector2(_speed * Time.deltaTime, 0);
+        if (_body.velocity.x > _maxSpeed)
+            _body.velocity = new Vector2(_maxSpeed, _body.velocity.y);
+
+        
+        if (Input.GetMouseButtonDown(0))
+            _body.AddForce(Vector2.up * _jumpForce, ForceMode2D.Impulse);
     }
 }
