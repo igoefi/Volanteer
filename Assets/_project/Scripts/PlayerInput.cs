3822efe50c30ea01ce82aaea11e13ba2d8a60325
igoefi <96096968+igoefi@users.noreply.github.com>
2022-10-15 13:57:12 +0300

    Update

--- Assets/_project/Scripts/PlayerInput.cs
+++ Assets/_project/Scripts/PlayerInput.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,10 +6,60 @@ using UnityEngine.Events;
 
 public class PlayerInput : MonoBehaviour
 {
-    public static UnityEvent TapEvent = new();
+    public static UnityEvent TapEvent { get; private set; } = new();
+    public static UnityEvent LeftSwipeEvent { get; private set; } = new();
+    public static UnityEvent DownSwipeEvent { get; private set; } = new();
+
+    private Vector2 _firstTouchVector;
+    private Vector2 _lastTouchVector;
+
+    [SerializeField] float lengthXSwipe;
+    [SerializeField] float lengthYSwipe;
+
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
-            TapEvent.Invoke();
+        {
+            _firstTouchVector = Input.mousePosition;
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            _lastTouchVector = Input.mousePosition;
+            CheckSwipe();
+        }
+    }
+
+    private void CheckSwipe()
+    {
+        float xDistance = _firstTouchVector.x - _lastTouchVector.x;
+        float yDistance = _firstTouchVector.y - _lastTouchVector.y;
+
+        if (xDistance < lengthXSwipe && yDistance < lengthYSwipe) 
+        {
+            Debug.Log("1");
+            TapEvent.Invoke(); 
+            return; 
+        }
+        Debug.Log('2');
+
+        UnityEvent xEvent = null;
+        UnityEvent yEvent = null;
+        UnityEvent needEvent;
+
+        if (xDistance >= lengthXSwipe)
+            xEvent = LeftSwipeEvent;
+        if (yDistance >= lengthYSwipe)
+            yEvent = DownSwipeEvent;
+
+
+        if (xEvent != null && yEvent != null)
+        {
+            needEvent = Math.Abs(xDistance) > Math.Abs(yDistance) ? xEvent : yEvent;
+        }
+
+        needEvent = xEvent != null ? xEvent : yEvent;
+
+        needEvent.Invoke();
     }
 }
